6044e587e7496d004e908e7bd2f479f4958ccb99
Tobias Odermatt <90388194+TobiasOdermatt@users.noreply.github.com>
2022-06-22 15:21:47 +0200

    dbconnector.inc.php hinzugefügt. Zugang muss geändert werden
    + Der Administrator bekommt einen MySQL Admin zugriff
    + Der Benutzer bekommt einen MySQL User zugriff

---
+++ Projektordner/lib/dbconnector.inc.php
@@ -0,0 +1,26 @@
+<?php
+if (isset($_SESSION["role"])){ 
+	$_SESSION["role"] == "Admin" ? $conn = admin_connect() : $conn = user_connect();
+} else{$conn = user_connect();}
+
+		function admin_connect(){
+			$host     = 'localhost';       
+			$username = 'root';            
+			$password = '';      		  
+			$dbname = 'ToDo';   			
+		$conn = mysqli_connect($host, $username, $password, $dbname);
+		if (!$conn) {
+			die("Verbindung mit der Datenbank nicht möglich, kontaktieren Sie den Seitenadministrator.");
+		  }return $conn;}
+
+		function user_connect(){
+			$host     = 'localhost';       
+			$username = 'root';            
+			$password = '';      		 
+			$dbname = 'ToDo'; 
+		$conn = mysqli_connect($host, $username, $password, $dbname);
+		if (!$conn) {
+			die("Verbindung mit der Datenbank nicht möglich, kontaktieren Sie den Seitenadministrator.");
+		  }return $conn;}
+		
+?>
