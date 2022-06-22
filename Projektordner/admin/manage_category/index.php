5b862ff63087937a55eb3030585d1cbb521b8433
Tobias Odermatt <90388194+TobiasOdermatt@users.noreply.github.com>
2022-06-22 15:25:05 +0200

    Kategorie kann angesehen und erstellt, bearbeitet werden

---
+++ Projektordner/admin/manage_category/index.php
@@ -0,0 +1,157 @@
+<?php
+  session_start();
+  session_regenerate_id();
+  $edit_name = "";
+  if (!isset($_SESSION["user"]) || $_SESSION["role"] != "admin"){ //Ist die Session nicht gesetzt oder der Nutzer kein Admin wird man auf die Login Seite weitergeleitet.
+    header("location:../../login");}
+    require("../../lib/dbconnector.inc.php");
+    $site_title = "Kategorie hinzufügen.";
+    $alert_class_name = "alert alert-info";
+    $alert_message = "Hier können Sie eine neue Kategorie anlegen.";
+    $error = "";
+    $editmode = "submit"; //Wert von Submit Button, wird ein GET-Request gesendet wird zu ID geändert
+
+function categoryExists($category,$conn){ //Überprüft ob eine Kategorie unter dem Namen schon existiert
+    $stmt = $conn->prepare("SELECT name FROM category WHERE name = (?)");
+    $stmt->bind_param("s",$category);
+    $stmt->execute();
+    $stmt->store_result();
+    $countRows = $stmt->num_rows;
+    if($countRows >= 1){return true;}
+    else{return false;}
+}
+function existID($ID,$conn){ //Überprüft ob die ID einer Kategorie  existiert
+    $stmt = $conn->prepare("SELECT category_id FROM category WHERE category_id = (?)");
+    $stmt->bind_param("i",$ID);
+    $stmt->execute();
+    $stmt->store_result();
+    $countRows = $stmt->num_rows;
+    if($countRows >= 1){return true;}
+    else{return false;}
+  }
+function delEveryAssign ($conn, $category_id){
+    $stmt = $conn->prepare("DELETE FROM user_has_categories WHERE category_category_id = (?)");
+    $stmt->bind_param("s",$category_id);
+    $stmt->execute();
+  }
+
+function delCategory($ID,$conn){ //Löscht eine Kategorie/sowie dazu gehörigen ToDos, die ID muss mitgegeben werden
+    if(existID($ID,$conn)){ //Überprüft ob die ID existiert
+        delEveryAssign($conn,$ID);
+        $stmt = $conn->prepare("DELETE FROM category WHERE category_id = (?)");
+        $stmt->bind_param("s",$ID);
+        $stmt->execute();
+        $stmt = $conn->prepare("DELETE FROM to_do WHERE category_category_id = (?)");
+        $stmt->bind_param("s",$ID);
+        $stmt->execute();
+        $stmt->store_result();
+        header("location: ../view_category?status=delsuccess"); //Bei Erfolg wird dies der view_category Seite mitgeteilt
+      }else{"location: ../view_category?status=delfail";} //Existiert die ID nicht wird dies der view_category Seite mitgeteilt
+}
+function editCategory($ID,$conn){ //Lädt die zu bearbeitende Kategorie, ID wird mitgegeben
+    if(existID($ID,$conn)){ //Überprüft ob die ID existiert.
+        global $edit_name, $alert_message,$site_title;
+        $sql = "SELECT name,category_id from category where category_id = '$ID'";
+        $result = $conn->query($sql);
+        $row = $result->fetch_assoc();
+        $edit_name = $row['name'];
+        $site_title = "Kategorie bearbeiten";
+        $alert_message = "Sie können die Kategorie mit der ID #".$ID." bearbeiten";
+      }
+}
+
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+  require("../../lib/validate_form_server_side.inc.php");
+  validateCategory();
+          if(empty($error)){
+            if(isset($_POST['writemode'])){//Dient zur unterscheidung von Kategorie erstellen und Bearbeiten Modus
+                if($_POST['writemode'] == "submit"){//Beim erstellen
+                    if(!(categoryExists($category,$conn))){ //Überprüft ob der Name existiert, falls ja wird dies ausgegeben.
+                        $stmt = $conn->prepare("INSERT INTO category (name) VALUES (?)");
+                        $stmt->bind_param("s", $category);
+                        $stmt->execute();
+                        $stmt->close();
+                        $conn->close();
+                        $alert_class_name = "alert alert-success";$alert_message = "Die Kategorie wurde erfolgreich angelegt.";}
+                        else $error .= "Kategorie existiert bereits.<br/>";}
+                else{
+                    $tempID = (int)$_POST['writemode'];// Beim bearbeiten
+                    if(existID($tempID,$conn)){ //Überprüft ob die ID existiert.
+                    $stmt = $conn->prepare("UPDATE category SET name = (?) WHERE category_id = (?)");
+                    $stmt->bind_param("si", $category, $tempID);
+                    $stmt->execute();
+                    $stmt->close();
+                    $conn->close();
+                    $alert_class_name = "alert alert-success";$alert_message = "Die Kategorie wurde erfolgreich bearbeitet.";}}
+            }
+          }else{$alert_message = $error;}}
+
+if($_SERVER['REQUEST_METHOD'] == 'GET') //Sollte eine Kategorie bearbeitet oder gelöscht werden
+{if(isset($_GET['ID'])){         //Sollte eine Kategorie bearbeitet werden, dann ist die Variabel ID gesetzt
+  $editmode = (int)$_GET['ID']; //Wird zu INT konvertiert.
+    editCategory($editmode,$conn);
+}
+elseif(isset($_GET['DEL'])){  //Sollte ein Beitrag gelöscht werden, dann ist die Variabel DEL gesetzt.
+    $delID = (int)$_GET['DEL']; //Um SQL Injections zu verhindern
+    delCategory($delID,$conn);}}  //Funktion für die Löschung wird gestartet, die Variabel DEL wird mitgegeben.
+    if(!(empty($error))){$alert_class_name = "alert alert-danger"; $alert_message = $error;} //Fehler werden ausgeben
+    
+
+?>
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <title>Startseite</title>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+            <link rel="stylesheet" href="../../CSS/stylesheet.css">
+            <link rel="icon" type="image/x-icon" href="../../img/favicon.png">
+</head>
+        <body>
+        <nav class="navbar navbar-default">
+             <div class="container-fluid">
+             <div class="nav navbar-nav navbar-left">
+             <div class="panel-buttons">
+             <form style="display: inline" action="../index.php" method="get">
+            <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-home"></span> Home | Benutzerverwaltung</button></form>
+             </div>  </div>
+              
+             <div class="nav navbar-nav navbar-right">
+                        <div class="panel-buttons">
+
+            <form style="display: inline" action="../view_category" method="get">
+            <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-inbox"></span> Kategorien ansehen</button></form>
+
+            <form style="display: inline" action="../manage_category/" method="get">
+            <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-tag"> </span> Kategorie erstellen</button></form>
+
+            <form style="display: inline" action="../manage_user" method="get">
+            <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-user"></span> Benutzer erstellen</button></form>
+
+            <form style="display: inline" action="../../logout" method="get">
+            <button type="submit" class="btn btn-default "><span class="glyphicon glyphicon-log-out"></span> Log out</button></form>
+
+                       </div>
+             </div>
+            </div>
+    </nav>
+    <div class="well well-sm"><h4><?php echo $site_title;?></h4>  </div>
+    <div class="container">
+    <div class="form-group"><div class="<?php echo $alert_class_name;?>" role="alert"><?php echo $alert_message; ?></div></div>
+        <form action="index.php" method="post">
+        <!-- Kategorie -->
+        <div class="form-group">
+          <label for="category">Kategoriename *</label>
+          <input type="text" name="category" class="form-control" id="category"
+                  value="<?php echo $edit_name ?>"
+                  placeholder="Geben Sie eine Kategorie ein, maximal 25 Zeichen."
+                  maxlength="25"
+                  required>
+        </div>
+        <button type="submit" name="writemode" value="<?php echo $editmode;?>" class="btn btn-info">Senden</button>
+        <button type="reset" name="button" value="reset" class="btn btn-warning">Löschen</button>
+</form>
+   
+</div>
+        </body>
+</html>
---
+++ Projektordner/admin/view_category/index.php
@@ -0,0 +1,119 @@
+<?php
+  session_start();
+  session_regenerate_id();
+  if (!isset($_SESSION["user"]) || $_SESSION["role"] != "admin"){ //Ist die Session nicht gesetzt oder der Nutzer kein Admin wird man auf die Login Seite weitergeleitet.
+    header("location:../../login");}
+    require("../../lib/dbconnector.inc.php"); //Datenbank
+    $site_title = "Adminpanel / Kategorieverwaltung"; //Standart Seitentitel
+    $alert_class_name = "alert alert-success";
+    $alert_message = "Hier können alle Kategorien eingesehen werden."; //Standart Seitentext
+
+    if(countCategory($conn) == 0){ // Existiert keine Kategorie wird dies angezeigt
+        $alert_class_name = "alert alert-warning";
+        $alert_message = "Es existieren noch keine Kategorieeinträge.";
+        }
+
+    function countCategory($conn){ //Gibt die Anzahl der Kategorien zurück
+        $sql = "SELECT COUNT(*) from category";
+        $result = $conn->query($sql);
+        $data =  $result->fetch_assoc();
+        return $data['COUNT(*)'];
+    }
+    function view_category($conn){  //Gibt alle Nutzer in einer Tabelle aus.
+        $query = $conn->query("SELECT *  FROM category");
+        while($row = $query->fetch_array()){
+            echo "<tr>".
+            "<td>".$row['category_id']."</td>".
+             "<td>".$row['name']."</td>".
+             "<td style=\"width: 20%;\">".
+             "<a href=\"../assign_category_user/?ID=".$row['category_id']."\">".//Button für das zuweisen
+             "<span class=\"fa-stack\">".
+             "<i class=\"fa fa-square fa-stack-2x\"></i>".
+             "<i class=\"fa fa-user-plus fa-stack-1x fa-inverse\"></i>". //Icon Zuweisen
+             "</span></a>".
+             "<a href=\"../manage_category?ID=".$row['category_id']."\">".//Button für das bearbeiten
+             "<span class=\"fa-stack\">".
+             "<i class=\"fa fa-square fa-stack-2x\"></i>".
+             "<i class=\"fa fa-pencil fa-stack-1x fa-inverse\"></i>". //Icon bearbeiten
+             "</span></a>".
+             "<a href=\"../manage_category?DEL=".$row['category_id']."\">".//Button für das löschen
+              "<span class=\"fa-stack\">".
+                    "<i class=\"fa fa-square fa-stack-2x\"></i>".
+                     "<i class=\"fa fa-trash-o fa-stack-1x fa-inverse\"></i>". //Icon löschen
+             "</span></a></td>".
+             "</tr>";
+            }
+    }
+
+if(isset($_GET['status'])){//Wird ein GET-Request mit Status Attribut gesendet wird eine Meldung Ausgegeben
+    if($_GET['status'] == "delsuccess"){//Meldung wenn die Kategorie erfolgreich gelöscht wurde
+    $alert_class_name = "alert alert-success";
+     $alert_message = "Die Kategorie wurde erfolgreich gelöscht.";
+    }
+    else if($_GET['status'] == "delfail"){//Meldung wenn die Kategorie nicht existiert.
+        $alert_class_name = "alert alert-danger";
+        $alert_message = "Die Kategorie existiert nicht.";
+    }
+  }
+?>
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <title>Startseite</title>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+            <link rel="stylesheet" href="../../CSS/stylesheet.css">
+            <link rel="icon" type="image/x-icon" href="../../img/favicon.png">
+</head>
+        <body>
+        </head>
+        <body>
+        <nav class="navbar navbar-default">
+             <div class="container-fluid">
+             <div class="nav navbar-nav navbar-left">
+             <div class="panel-buttons">
+             <form style="display: inline" action="../" method="get">
+            <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-home"></span> Home | Benutzerverwaltung</button></form>
+             </div>  </div>
+              
+             <div class="nav navbar-nav navbar-right">
+                        <div class="panel-buttons">
+
+            <form style="display: inline" action="index.php" method="get">
+            <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-inbox"></span> Kategorien ansehen</button></form>
+
+            <form style="display: inline" action="../manage_category" method="get">
+            <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-tag"> </span> Kategorie erstellen</button></form>
+
+            <form style="display: inline" action="../manage_user" method="get">
+            <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-user"></span> Benutzer erstellen</button></form>
+
+            <form style="display: inline" action="../../logout" method="get">
+            <button type="submit" class="btn btn-default "><span class="glyphicon glyphicon-log-out"></span> Log out</button></form>
+
+                       </div>
+             </div>
+            </div>
+    </nav>
+    
+    <div class="well well-sm"><h4><?php echo $site_title;?></h4>  </div>
+    <div class="container">
+    <div class="form-group"><div class="<?php echo $alert_class_name;?>" role="alert"><?php echo $alert_message; ?></div></div>
+
+        <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>ID:</th>
+                        <th>Name:</th>						
+                        <th>Aktion:</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <?php  view_category($conn);?>
+                </tbody>
+            </table>
+
+</div>
+        </body>
+</html>
