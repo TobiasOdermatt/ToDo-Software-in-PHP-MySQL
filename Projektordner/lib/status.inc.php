a5efeb57aa1922d71456e82f0938660e2495f7ad
Tobias Odermatt <90388194+TobiasOdermatt@users.noreply.github.com>
2022-06-22 15:32:12 +0200

    Eigenes Fehler- Warnungsmanagement hinzugefügt
    Verschiedene Meldungen können nun dem Benutzer ausgegeben werden.

---
+++ Projektordner/lib/status.inc.php
@@ -0,0 +1,29 @@
+<?php
+if(isset($_GET['status'])){ //Wird ein GET-Request mit Status Attribut gesendet wird eine Meldung Ausgegeben
+
+    //Meldung wenn der Nutzer erfolgreich gelöscht wurde
+    if($_GET['status'] == "delsuccess"){ 
+        $alert_class_name = "alert alert-success";
+        $alert_message = "Das ToDo wurde erfolgreich gelöscht.";}
+
+    //Meldung falls ToDo nicht existiert oder Nutzer keine Berechtigung hat
+    else if($_GET['status'] == "permissionfail"){
+        $alert_class_name = "alert alert-danger";
+        $alert_message = "Sie haben keine Berechtigung für dieses ToDo";
+    }
+    //Meldung falls ToDo Archiviert
+    else if($_GET['status'] == "archieved"){ 
+        $alert_class_name = "alert alert-success";
+        $alert_message = "ToDo wurde archieviert";
+    }
+    //Meldung falls ToDo nicht mehr Archiviert ist
+    else if($_GET['status'] == "unarchieved"){ 
+        $alert_class_name = "alert alert-info";
+        $alert_message = "ToDo wurde unarchieviert";
+    }}
+    if(getAllowedToDoIDs($conn,$user_ID) == null){ //Sind keine ToDos vorhanden
+        $alert_class_name = "alert alert-warning";
+        $alert_message = "Es existieren noch keine ToDos";
+        }
+    
+?>
