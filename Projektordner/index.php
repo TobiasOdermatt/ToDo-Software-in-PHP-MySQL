a85db9fcfd4fa33283887d2b4b8b705b213ffed1
Tobias Odermatt <90388194+TobiasOdermatt@users.noreply.github.com>
2022-06-22 15:19:15 +0200

    Startseite hinzugefügt

---
+++ Projektordner/index.php
@@ -0,0 +1,33 @@
+<?php
+  session_start();
+  session_regenerate_id();
+?>
+<html>
+    <head>
+        <title>Startseite</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="icon" type="image/x-icon" href="img/favicon.png">
+    <head>
+    <body>
+      
+    <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse">
+    </div>
+  </div>
+</nav>
+<div class="well well-sm"><h4>Startseite</h4></div>
+      <div class="text-center">
+          <a href="login"><button type="submit" class="btn btn-default">ToDoX Applikation starten</button></a>
+      </div>
+    </form>
+
+    <footer class="footer">
+</footer>
+</div>
+
+</body>
+</html>
